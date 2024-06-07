56dae03c417a7b5ecd40d230f988df1bfab055e8
Bryan Almeida <124791559+BryanAlmeida016@users.noreply.github.com>
2024-06-07 12:45:14 -0300

    Add files via upload

--- Produtora_de_vinhos/produtoravinho.sql
+++ Produtora_de_vinhos/produtoravinho.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24-Maio-2024 às 21:59
--- Versão do servidor: 10.4.32-MariaDB
--- versão do PHP: 8.2.12
+-- Tempo de geração: 07/06/2024 às 17:44
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,18 +26,18 @@ USE `produtoravinho`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `casta`
+-- Estrutura para tabela `casta`
 --
 
 CREATE TABLE `casta` (
-  `codCasta` int(11) NOT NULL,
+  `casta_id` int(11) NOT NULL,
   `casta` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `casta_vinho`
+-- Estrutura para tabela `casta_vinho`
 --
 
 CREATE TABLE `casta_vinho` (
@@ -48,38 +48,56 @@ CREATE TABLE `casta_vinho` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produtor`
+-- Estrutura para tabela `produtor`
 --
 
 CREATE TABLE `produtor` (
-  `produtorid` int(11) NOT NULL,
-  `nomeProdutor` varchar(50) DEFAULT NULL,
+  `produtor_id` int(11) NOT NULL,
+  `produtor` varchar(50) DEFAULT NULL,
   `moradaProdutor` varchar(50) DEFAULT NULL,
   `telefone` varchar(15) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `codregiao` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `produtor`
+--
+
+INSERT INTO `produtor` (`produtor_id`, `produtor`, `moradaProdutor`, `telefone`, `email`, `codregiao`) VALUES
+(1, 'Qt.Vallado', 'Régua', '254323147', 'vallado@mail.telepac.pt', 1);
+
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `regiao`
+-- Estrutura para tabela `regiao`
 --
 
 CREATE TABLE `regiao` (
   `idregiao` int(11) NOT NULL,
-  `descriregiao` varchar(50) NOT NULL
+  `regiao` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `regiao`
+--
+
+INSERT INTO `regiao` (`idregiao`, `regiao`) VALUES
+(1, 'Dão'),
+(2, 'Douro'),
+(3, 'Alenteijo'),
+(4, 'ETECMCM'),
+(5, 'Fazenda da Etec');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `vinho`
+-- Estrutura para tabela `vinho`
 --
 
 CREATE TABLE `vinho` (
-  `vinhoid` int(11) NOT NULL,
-  `nomeVinho` varchar(30) DEFAULT NULL,
+  `vinho_id` int(11) NOT NULL,
+  `vinho` varchar(30) DEFAULT NULL,
   `ano_vinho` int(11) DEFAULT NULL,
   `cor` varchar(15) DEFAULT NULL,
   `grau` decimal(7,2) DEFAULT NULL,
@@ -92,89 +110,89 @@ CREATE TABLE `vinho` (
 --
 
 --
--- Índices para tabela `casta`
+-- Índices de tabela `casta`
 --
 ALTER TABLE `casta`
-  ADD PRIMARY KEY (`codCasta`);
+  ADD PRIMARY KEY (`casta_id`);
 
 --
--- Índices para tabela `casta_vinho`
+-- Índices de tabela `casta_vinho`
 --
 ALTER TABLE `casta_vinho`
   ADD KEY `codVinho` (`codVinho`),
   ADD KEY `codCasta` (`codCasta`);
 
 --
--- Índices para tabela `produtor`
+-- Índices de tabela `produtor`
 --
 ALTER TABLE `produtor`
-  ADD PRIMARY KEY (`produtorid`),
+  ADD PRIMARY KEY (`produtor_id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD KEY `codregiao` (`codregiao`);
 
 --
--- Índices para tabela `regiao`
+-- Índices de tabela `regiao`
 --
 ALTER TABLE `regiao`
   ADD PRIMARY KEY (`idregiao`);
 
 --
--- Índices para tabela `vinho`
+-- Índices de tabela `vinho`
 --
 ALTER TABLE `vinho`
-  ADD PRIMARY KEY (`vinhoid`),
+  ADD PRIMARY KEY (`vinho_id`),
   ADD KEY `codProdutor` (`codProdutor`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `casta`
 --
 ALTER TABLE `casta`
-  MODIFY `codCasta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `casta_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `produtor`
 --
 ALTER TABLE `produtor`
-  MODIFY `produtorid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `produtor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `regiao`
 --
 ALTER TABLE `regiao`
-  MODIFY `idregiao` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idregiao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `vinho`
 --
 ALTER TABLE `vinho`
-  MODIFY `vinhoid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `vinho_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `casta_vinho`
+-- Restrições para tabelas `casta_vinho`
 --
 ALTER TABLE `casta_vinho`
-  ADD CONSTRAINT `casta_vinho_ibfk_1` FOREIGN KEY (`codVinho`) REFERENCES `vinho` (`vinhoid`),
-  ADD CONSTRAINT `casta_vinho_ibfk_2` FOREIGN KEY (`codCasta`) REFERENCES `casta` (`codCasta`);
+  ADD CONSTRAINT `casta_vinho_ibfk_1` FOREIGN KEY (`codVinho`) REFERENCES `vinho` (`vinho_id`),
+  ADD CONSTRAINT `casta_vinho_ibfk_2` FOREIGN KEY (`codCasta`) REFERENCES `casta` (`casta_id`);
 
 --
--- Limitadores para a tabela `produtor`
+-- Restrições para tabelas `produtor`
 --
 ALTER TABLE `produtor`
   ADD CONSTRAINT `produtor_ibfk_1` FOREIGN KEY (`codregiao`) REFERENCES `regiao` (`idregiao`);
 
 --
--- Limitadores para a tabela `vinho`
+-- Restrições para tabelas `vinho`
 --
 ALTER TABLE `vinho`
-  ADD CONSTRAINT `vinho_ibfk_1` FOREIGN KEY (`codProdutor`) REFERENCES `produtor` (`produtorid`);
+  ADD CONSTRAINT `vinho_ibfk_1` FOREIGN KEY (`codProdutor`) REFERENCES `produtor` (`produtor_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
